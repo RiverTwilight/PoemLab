fb8d2393ae7d3b4c0c99037e9a0bad9bc9ea5aa6
RiverTwilight <wrj2014@126.com>
2021-02-15 14:42:24 +0800

    test

--- api/login.js
+++ api/login.js
@@ -8,14 +8,12 @@ function createUser(params) {
   return false
 }
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   const { jscode } = req.query;
 
   const code = await axios.get(`https://api.weixin.qq.com/sns/jscode2session?appid=${process.env.APPID}&secret=${process.env.SECRET}&js_code=${jscode}&grant_type=authorization_code`);
 
   res.json({
-    body: code,
-    query: req.query,
-    cookies: req.cookies,
+    body: code
   });
 };
